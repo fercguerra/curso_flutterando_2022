cb2ed91cec5272cc45ddb78fc02fd2b3949a6259
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-08-17 10:56:11 -0300

    Componetizando Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'myapp.dart';
+
 //widget: representa toda a tela do celular ou uma parte dela, em formato de árvore, ou seja, um dentro do outro
 
 //Flutter é declarativo pois utiliza linguagem de programação e não de marcação para desenhar telas
@@ -9,29 +11,5 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
-  }
-}
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(child: Text('Flutterando')),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Clicou');
-        },
-      ),
-    );
-  }
-}
